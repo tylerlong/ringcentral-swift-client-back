1598970f112aa5fd702c8c5e11fe41d13a9495db
Tyler Long <tyler4long@gmail.com>
2016-09-29 22:33:45 +0800

    Update http methods signature.

--- Source/Http.swift
+++ Source/Http.swift
@@ -20,7 +20,7 @@ public struct HTTPError {
 extension RestClient {
 
     // create a new Alamofire request object
-    func newRequest(_ endpoint: String, method: HTTPMethod, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil) -> DataRequest {
+    func newRequest(_ endpoint: String, method: HTTPMethod, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil) -> DataRequest {
         let urlString = URL(string: server)!.appendingPathComponent(endpoint).absoluteString
         var headers = headers ?? [:]
         if token != nil && headers["Authorization"] == nil {
@@ -31,7 +31,7 @@ extension RestClient {
     }
 
     // request Data
-    open func requestData(_ endpoint: String, method: HTTPMethod, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ data: Data?, _ error: HTTPError?) -> Void) {
+    open func requestData(_ endpoint: String, method: HTTPMethod, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ data: Data?, _ error: HTTPError?) -> Void) {
         let request = newRequest(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers)
         request.responseData { response in
             let statusCode = response.response!.statusCode
@@ -44,7 +44,7 @@ extension RestClient {
     }
 
     // request String
-    open func requestString(_ endpoint: String, method: HTTPMethod, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
+    open func requestString(_ endpoint: String, method: HTTPMethod, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
         let request = newRequest(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers)
         request.responseString { response in
             let statusCode = response.response!.statusCode
@@ -55,22 +55,22 @@ extension RestClient {
             }
         }
     }
-    open func getString(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
+    open func getString(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
         requestString(endpoint, method: .get, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
-    open func postString(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
+    open func postString(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
         requestString(endpoint, method: .post, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
-    open func putString(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
+    open func putString(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
         requestString(endpoint, method: .put, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
-    open func deleteString(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
+    open func deleteString(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ string: String?, _ error: HTTPError?) -> Void) {
         requestString(endpoint, method: .delete, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
 
 
     // request model
-    open func request<T: Mappable>(_ endpoint: String, method: HTTPMethod, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
+    open func request<T: Mappable>(_ endpoint: String, method: HTTPMethod, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
         requestString(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers) { string, error in
             if error != nil {
                 callback(nil, error)
@@ -79,16 +79,16 @@ extension RestClient {
             }
         }
     }
-    open func get<T: Mappable>(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
+    open func get<T: Mappable>(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
         request(endpoint, method: .get, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
-    open func post<T: Mappable>(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
+    open func post<T: Mappable>(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
         request(endpoint, method: .post, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
-    open func put<T: Mappable>(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
+    open func put<T: Mappable>(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
         request(endpoint, method: .put, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
-    open func delete<T: Mappable>(_ endpoint: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: [String: String]? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
+    open func delete<T: Mappable>(_ endpoint: String, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, callback: @escaping (_ t: T?, _ error: HTTPError?) -> Void) {
         request(endpoint, method: .delete, parameters: parameters, encoding: encoding, headers: headers, callback: callback)
     }
 
