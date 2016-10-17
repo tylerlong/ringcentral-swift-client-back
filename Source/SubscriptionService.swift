2486d5e1cde55ff22637c3fb102331a80a74ea11
Tyler Long <tyler4long@gmail.com>
2016-10-17 14:59:10 +0800

    Add PubNub library

--- Source/SubscriptionService.swift
+++ Source/SubscriptionService.swift
@@ -1,10 +1,49 @@
-open class SubscriptionService {
+import PubNub
 
+open class SubscriptionService: NSObject, PNObjectEventListener {
+
+    var rc: RestClient!
+    var pubnub: PubNub?
+    var subscriptionInfo: SubscriptionInfo?
     public var eventFilters: [String] = []
     public var callback: ((String) -> Void)? = nil
 
+    public init(_ rc: RestClient) {
+        self.rc = rc
+    }
+
+    public func client(_ client: PubNub, didReceiveMessage message: PNMessageResult) {
+        let base64Message = message.data.message as! String
+        print(base64Message)
+    }
+
     open func register() {
-        
+        if alive() {
+            renew()
+        } else {
+            subscribe()
+        }
+    }
+
+    open func subscribe() {
+
+    }
+
+    open func renew() {
+
+    }
+
+    func alive() -> Bool {
+        if let _ = pubnub, let subscriptionInfo = subscriptionInfo {
+            if subscriptionInfo.id != nil {
+                if let deliveryMode = subscriptionInfo.deliveryMode {
+                    if deliveryMode.subscriberKey != nil && deliveryMode.address != nil {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
     }
 
 }
@@ -13,7 +52,7 @@ open class SubscriptionService {
 extension Subscription {
 
     public func new() -> SubscriptionService {
-        return SubscriptionService()
+        return SubscriptionService(rc)
     }
 
 }
