56605436438bae50a26e4577ade29160d8921395
Tyler Long <tyler4long@gmail.com>
2016-11-07 10:38:49 +0800

    Add ServiceInfoTest

---
+++ Tests/ServiceInfoTest.swift
@@ -0,0 +1,38 @@
+import XCTest
+import RingCentral
+
+
+class ServiceInfoTest: BaseTest {
+
+    func testAccountServiceInfo() {
+        let expectation1 = expectation(description: "expectation1")
+
+        rc.restapi("v1.0").account("~").serviceInfo().get() { serviceInfo, error in
+            XCTAssertNil(error)
+            let feature = serviceInfo!.serviceFeatures![0]
+            XCTAssertTrue("FaxReceiving" == feature.featureName!)
+            XCTAssertTrue(true == feature.enabled!)
+            expectation1.fulfill()
+        }
+
+        waitForExpectations(timeout: 10) { error in
+            XCTAssertNil(error)
+        }
+    }
+
+    func testMeetingServiceInfo() {
+        let expectation1 = expectation(description: "expectation1")
+
+        rc.restapi("v1.0").account("~").extension("~").meetingServiceInfo().get() { serviceInfo, error in
+            // todo: update code below after getting permission
+            XCTAssertNotNil(error)
+            XCTAssertTrue(403 == error!.statusCode)
+            expectation1.fulfill()
+        }
+
+        waitForExpectations(timeout: 10) { error in
+            XCTAssertNil(error)
+        }
+    }
+
+}
