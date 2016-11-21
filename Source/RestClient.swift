6f91d18bb8d41289bc5cdc4922be83e19b7100bf
Tyler Long <tyler4long@gmail.com>
2016-11-21 18:52:18 +0800

    Support 3-legged oauth

--- Source/RestClient.swift
+++ Source/RestClient.swift
@@ -97,6 +97,35 @@ open class RestClient {
         }
     }
 
+    open func authorizeUri(redirectUri: String, state: String = "") -> String {
+        var urlComponents = URLComponents(string: server)!
+        urlComponents.path = "/restapi/oauth/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "state", value: state),
+            URLQueryItem(name: "redirect_uri", value: redirectUri),
+            URLQueryItem(name: "client_id", value: appKey),
+        ]
+        return urlComponents.string!
+    }
+
+    open func authorize(authCode: String, redirectUri: String, callback: ((_ token: TokenInfo?, _ error: HTTPError?) -> Void)? = nil) {
+        let parameters: [String: String] = [
+            "grant_type": "authorization_code",
+            "redirect_uri": redirectUri,
+            "code": authCode,
+        ]
+        let headers: [String: String] = ["Authorization": basicAuthToken()]
+        postString("/restapi/oauth/token", parameters: parameters, encoding: URLEncoding.default, headers: headers) { string, error in
+            if error == nil {
+                self.token = TokenInfo(JSONString: string!)
+                callback?(self.token, nil)
+            } else {
+                callback?(nil, error)
+            }
+        }
+    }
+
     open func restapi(_ _id: String? = "v1.0") -> RestapiPath {
         return RestapiPath(parent: PathSegment(parent: nil, _id: nil, rc: self), _id: _id)
     }
